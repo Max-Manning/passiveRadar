bb76650cc7bb2cf447b3d79e3d5f06a9e680ebc8
Max Manning <macksmanning@gmail.com>
2019-11-21 22:03:55 -0400

    streamlined run script

--- PRalgo.py
+++ PRalgo.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.signal as signal
-from PRutils import shift, shift_circ
+from signal_utils import shift, shift_circ
 
 
 def find_channel_offset(s1, s2, nd=32, nl=100):
@@ -10,19 +10,38 @@ def find_channel_offset(s1, s2, nd=32, nl=100):
     xc = np.abs(signal.correlate(B1, B2, mode='valid'))
     return (np.argmax(xc) - nl)*nd
 
+def offset_compensation(x1, x2, ndec):
+    s1 = x1[0:1000000]
+    s2 = x2[0:1000000]
+    os = find_channel_offset(s1, s2, ndec, 6*ndec)
+    if(os == 0):
+        return x2
+    else:
+        return shift(x2, os)
+
 
 def fast_xambg(s1, s2, nlag, nfft):
-    ''' algorithm from Howland (2005): FM radio based bistatic radar'''
+    ''' Fast Cross-Ambiguity Fuction
+    
+    Parameters:
+        s1, s2: input vectors for cross-ambiguity function
+        nlag: number of lag bins to compute
+        nfft: number of doppler bins to compute (should be power of 2)
+    Returns:
+        xc: (nfft, nlag+1, 1) matrix containing cross-ambiguity surface
+        third dimension added for easy stacking in dask
+
+    '''
     if s1.shape != s2.shape:
-        raise ValueError('shape mismatch!!!1')
+        raise ValueError('Vectors must be the same length')
     ndecim = int(s1.shape[0]/nfft)
-    xc = np.zeros((nfft, nlag+1), dtype=np.complex64)
+    xambg = np.zeros((nfft, nlag+1, 1), dtype=np.complex64)
     s2c = np.conj(s2)
     for k, lag in enumerate(np.arange(-nlag, 1)):
         sd = shift_circ(s2c, lag)*s1
         sd = signal.resample_poly(sd, 1, ndecim)
-        xc[:, k] = np.fft.fftshift(np.fft.fft(sd, nfft))
-    return xc
+        xambg[:, k, 0] = np.fft.fftshift(np.fft.fft(sd, nfft))
+    return xambg
 
 
 def LS_Filter(s1, s2, nlag, reg):
--- PRrun.py
+++ PRrun.py
@@ -3,8 +3,8 @@ import yaml
 import h5py
 import dask.array as da
 from dask.diagnostics import ProgressBar
-from PRutils import decimate, deinterleave_IQ, frequency_shift, shift
-from PRalgo import fast_xambg, find_channel_offset, LS_Filter
+from signal_utils import decimate, deinterleave_IQ, frequency_shift
+from PRalgo import fast_xambg, find_channel_offset, LS_Filter, offset_compensation
 
 def pad_to_chunks(darray, chunklen):
     padlen = chunklen - np.mod(darray.shape[0], chunklen)
@@ -12,46 +12,22 @@ def pad_to_chunks(darray, chunklen):
     padded = da.concatenate([darray, pad], axis=0)
     return padded
 
-def offset_compensation(x1, x2, ndec):
-    s1 = x1[0:1000000]
-    s2 = x2[0:1000000]
-    os = find_channel_offset(s1, s2, ndec, 6*ndec)
-    if(os == 0):
-        return x2
-    else:
-        return shift(x2, os)
-
-def getXambg(s1, s2, nlag, nfft, nblocks):
-    cl = s1.shape[0]//nblocks
-    XX = np.zeros((nfft, nlag+1, nblocks), dtype=np.complex64)
-    for k in range(nblocks):
-        XX[:,:,k] = fast_xambg(s1[k*cl:(k+1)*cl], s2[k*cl:(k+1)*cl], nlag, nfft)
-    return XX
-
-def Apply_LS_Filter(s1, s2, nl, reg, nblocks=5):
-    '''Apply a LS filter to a dask chunk'''
-    Bl = 524288 // 2
-    y = np.zeros(s2.shape, dtype=np.complex64)
-
-    for j in range(nblocks):
-        yb = LS_Filter(s1[j*Bl:(j+1)*Bl], s2[j*Bl:(j+1)*Bl], nl, reg)
-        y[j*Bl:(j+1)*Bl] = yb
-
-    return y
-
 if __name__ == "__main__":
 
-    chunk_length = 25624400  # 512488*2*5*5 (# CPI samples)*(2 floats/complex)*(decimation factor)*(5 CPIs/chunk)
+    # GET CONFIG PARAMETERS
 
     yf = open('PRconfig.yaml', 'r')
     PRconfig = yaml.safe_load(yf)
     yf.close()
 
+    # chunk_length = 5124880  # 512488*2*5*5 (# CPI samples)*(2 floats/complex)*(decimation factor)*(5 CPIs/chunk)
+    chunk_length = PRconfig['blockLength']*2*10
+    
     # LOADING DATA
 
     h5file = h5py.File(PRconfig['inputFile'], 'r')
-    ref_data = da.from_array(h5file['/data/ref'],  chunks = (chunk_length,))
-    srv_data = da.from_array(h5file['/data/srv'], chunks = (chunk_length,))
+    ref_data = da.from_array(h5file[PRconfig['inputReferencePath']][0:9*chunk_length],  chunks = (chunk_length,))
+    srv_data = da.from_array(h5file[PRconfig['inputSurveillancePath']][0:9*chunk_length], chunks = (chunk_length,))
     
     # PADDING TO INTEGER NUMBER OF CHUNK LENGTHS
 
@@ -84,7 +60,7 @@ if __name__ == "__main__":
     # APPLY LS FILTER
 
     arg1 = (r3, s3o2, 120, 1)
-    SRV_CLEANED_1 = da.map_blocks(Apply_LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
+    SRV_CLEANED_1 = da.map_blocks(LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
 
     rs05p = da.map_blocks(frequency_shift, r3,  1, Fs0//10, dtype=np.complex64, chunks=(chunk_length//20,))
     rs05n = da.map_blocks(frequency_shift, r3, -1, Fs0//10, dtype=np.complex64, chunks=(chunk_length//20,))
@@ -92,27 +68,25 @@ if __name__ == "__main__":
     rs1n = da.map_blocks(frequency_shift, r3, -2, Fs0//10, dtype=np.complex64, chunks=(chunk_length//20,))
 
     arg1 = (rs05p, SRV_CLEANED_1, 120, 1)
-    SRV_CLEANED_2 = da.map_blocks(Apply_LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
+    SRV_CLEANED_2 = da.map_blocks(LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
 
     arg1 = (rs05n, SRV_CLEANED_2, 120, 1)
-    SRV_CLEANED_3 = da.map_blocks(Apply_LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
+    SRV_CLEANED_3 = da.map_blocks(LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
 
     arg1 = (rs1p, SRV_CLEANED_3, 120, 1)
-    SRV_CLEANED_4 = da.map_blocks(Apply_LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
+    SRV_CLEANED_4 = da.map_blocks(LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
 
     arg1 = (rs1n, SRV_CLEANED_4, 120, 1)
-    SRV_CLEANED_5 = da.map_blocks(Apply_LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
-
-
+    SRV_CLEANED_5 = da.map_blocks(LS_Filter, *arg1, dtype=np.complex64, chunks = (chunk_length//20,))
 
     # # COMPUTE CROSS-AMBIGUITY FUNCTION
 
-    # xarg = (r3, SRV_CLEANED_5, 200, 512, 5)
-    # XAMBG = da.map_blocks(getXambg, *xarg, dtype=np.complex64, chunks=(512,201,5))
+    xarg = (r3, SRV_CLEANED_5, 200, 512)
+    XAMBG = da.map_blocks(fast_xambg, *xarg, dtype=np.complex64, chunks=(512,201,1))
 
-    # f = h5py.File(PRconfig['outputFile'])
-    # d = f.require_dataset('/xambg', shape=XAMBG.shape, dtype=XAMBG.dtype)
+    f = h5py.File(PRconfig['outputFile'])
+    d = f.require_dataset('/xambg', shape=XAMBG.shape, dtype=XAMBG.dtype)
 
-    # with ProgressBar():
-    #     da.store(XAMBG, d)
-    # f.close()
+    with ProgressBar():
+        da.store(XAMBG, d)
+    f.close()
