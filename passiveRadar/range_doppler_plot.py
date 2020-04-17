a9a04dcff4e7fc3b83518d2d70db0440ef61b568
Max Manning <macksmanning@gmail.com>
2020-04-17 15:42:01 -0300

    updated plotting script

--- passiveRadar/range_doppler_plot.py
+++ passiveRadar/range_doppler_plot.py
@@ -17,12 +17,14 @@ def persistence(X, k, hold, decay):
     Returns:
     frame: (NxM matrix) frame k of the original stack with persistence added'''
     frame = np.zeros((X.shape[0], X.shape[1]))
-    for i in range(hold):
+
+    nf = min(k+1, hold)
+    for i in range(nf):
         if k-i >= 0:
             frame = frame + X[:,:,k-i]*decay**i
     return frame
 
-def CFAR(X, fw, gw, thresh):
+def CFAR_2D(X, fw, gw, thresh):
     '''constant false alarm rate target detection
     
     Parameters:
@@ -38,48 +40,84 @@ def CFAR(X, fw, gw, thresh):
     e2 = fw - e1 + 1
     Tfilt[e1:e2, e1:e2] = 0
 
-    CR = X / (signal.convolve2d(X, Tfilt, mode='same', boundary='wrap') + 1e-10)
+    CR = normalize(X) / (signal.convolve2d(X, Tfilt, mode='same', boundary='wrap') + 1e-10)
     return CR > thresh
+
+def CFAR_sequential(X, rx, ry, gx, gy, thresh = None, order='xy'):
+    
+    filtx = np.ones((rx,))/(rx - gx)
+    ex1 = (rx-gx)//2
+    ex2 = rx - ex1 + 1
+    filtx[ex1:ex2] = 0
+
+    filty = np.ones((ry,))/(ry - gy)
+    ey1 = (ry-gy)//2
+    ey2 = ry - ey1 + 1
+    filtx[ey1:ey2] = 0
+
+    if order == 'xy':
+
+        C1 = np.zeros(X.shape)
+        for i in range(X.shape[1]):
+            C1[:,i] = X[:,i] / np.convolve(X[:,i], filtx, mode='same')
+
+        C2 = np.zeros(X.shape)
+        for i in range(X.shape[0]):
+            C2[i,:] = C1[i,:] / np.convolve(C1[i,:], filty, mode='same')
+
+    else:
+
+        C1 = np.zeros(X.shape)
+        for i in range(X.shape[0]):
+            C1[i,:] = X[i,:] / np.convolve(X[i,:], filty, mode='same')
+
+        C2 = np.zeros(X.shape)
+        for i in range(X.shape[1]):
+            C2[:,i] = C1[:,i] / np.convolve(C1[:,i], filtx, mode='same')
+    
+    if thresh is None:
+        return C2
+    else:
+        return C2 > thresh
+
 
 if __name__ == "__main__":
 
     f = h5py.File('..\\XAMBG_1011_256Hz_test2.hdf5', 'r')
-    xambg = normalize(np.abs(f['/xambg'][:,:,:]))
+    xambg = np.abs(f['/xambg'])
     f.close()
 
     print("Loaded data!!")
-
-    # xambg = xambg[:,:,100:200]
 
     Nframes = xambg.shape[2]
 
     # CFAR filtering
     CF = np.zeros(xambg.shape)
     for i in range(Nframes):
-        CF[:,:,i] = CFAR(xambg[:,:,i], 20, 4, 3.5)
-    
 
-    print("Okay starting with the frame rendering now!!1")
+        # CFAR filtering using a 2D kernel
+        # CF[:,:,i] = CFAR_2D(xambg[:,:,i], 20, 5, 3.0)
+
+        # CFAR filtering along range and doppler separately (range first)
+        CF[:,:,i] = CFAR_sequential(xambg[:,:,i], 30, 30, 4, 6, None, 'yx')
+
     # plot each frame
     for kk in range(Nframes):
-        
-        svname = '..\\IMG2\\img_' + "{0:0=3d}".format(kk) + '.png'
-        
-        figure = plt.figure()
 
         data = persistence(CF, kk, 30, 0.91)
-        data = np.fliplr(data.T) # get the orientation rights
+        data = np.fliplr(data.T) # get the orientation right
+        
+        svname = '..\\IMG2\\img_' + "{0:0=3d}".format(kk) + '.png'
+        figure = plt.figure()
 
-        # crop to region of interest
-        # data = data[:, 64:448]
-
+        # get max and min values for color map
         vmn = np.percentile(data.flatten(), 1)
         vmx = 1.8*np.percentile(data.flatten(),99)
+
+        # plot
         plt.imshow(data,cmap = 'gnuplot2', vmin=vmn, vmax=vmx, extent = [-256,256,0,250])
         plt.ylabel('Bistatic Range (km)')
         plt.xlabel('Doppler Shift (Hz)')
-    #    plt.plot(np.abs(mframes[kk,:]))
-        # plt.show()
         
         plt.tight_layout()
         plt.savefig(svname, dpi=100)
