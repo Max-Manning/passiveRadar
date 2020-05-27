9f00dfda111582b7e4d76e16d2f4422a5b3f2104
Max Manning <macksmanning@gmail.com>
2020-05-27 13:35:56 -0300

    update signal utils

--- passiveRadar/signal_utils.py
+++ passiveRadar/signal_utils.py
@@ -6,12 +6,13 @@ def normalize(x):
     return x/np.mean(np.abs(x).flatten())
 
 def decimate(x, q):
-    '''decimate x by a factor of q'''
-    return signal.decimate(x, q, n=255, ftype='fir', axis=0, zero_phase=False)
+    '''decimate x by a factor of q with some settings that I like'''
+    return signal.decimate(x, q, 20*q, ftype='fir', axis=0, zero_phase=False)
 
-def deinterleave_IQ(interleaved_data):
-    '''convert interleaved samples to complex float'''
-    return interleaved_data[0:-1:2].astype(np.complex64) + 1j*interleaved_data[1::2].astype(np.complex64)
+def deinterleave_IQ(interleavedIQ):
+    '''convert interleaved IQ samples to complex64'''
+    interleavedIQ = np.array(interleavedIQ)
+    return (interleavedIQ[0:-1:2] + 1j*interleavedIQ[1::2]).astype(np.complex64)
 
 def frequency_shift(x, fc, Fs):
     '''frequency shift x by fc where Fs is the sample rate of x'''
@@ -19,7 +20,9 @@ def frequency_shift(x, fc, Fs):
     return x*np.exp(1j*2*np.pi*fc*nn/Fs)
 
 def xcorr(s1, s2, nlead, nlag):
-    return signal.correlate(s1, np.pad(s2, (nlag, nlead), mode='constant'), mode='valid')
+    ''' cross-correlated s1 and s2'''
+    return signal.correlate(s1, np.pad(s2, (nlag, nlead), mode='constant'),
+        mode='valid')
 
 def shift(x, n):
     '''shift x by n samples, pad with zeros'''
@@ -36,12 +39,40 @@ def shift(x, n):
         e[:n] = x[-n:]
         return e
 
-def shift_circ(x, n):
-    '''circularly shift x by n samples'''
-    if n == 0:
-        return x
+def offset_compensation(x1, x2, ns, ndec, nlag=2000):
+    '''Find and correct a constant time offset between two signals using a 
+    cross-correlation
+    
+    Parameters:
+        s1, s2:     Arrays containing the input signals
+        ns:         Number of samples to use for cross-correlation
+        ndec:       Decimation factor prior to cross-correlation
+        nlag:       Number of lag bins for cross-correlation
+    Returns:
+        x2s:        The signal x2 time-shifted so that it aligns with x1. Edges 
+                    are padded with zeros.         
+    '''
+    s1 = x1[0:int(ns)]
+    s2 = x2[0:int(ns)]
+
+    # cross-correlate to find the offset
+    os = find_channel_offset(s1, s2, ndec, nlag)
+
+    if(os == 0):
+        return x2
     else:
-        e = np.empty_like(x)
-        e[:n] = x[-n:]
-        e[n:] = x[:-n]
-        return e
+        return shift(x2, os)
+
+def find_channel_offset(s1, s2, nd, nl):
+    '''use cross-correlation to find channel offset in samples'''
+    B1 = signal.decimate(s1, nd)
+    B2 = np.pad(signal.decimate(s2, nd), (nl, nl), 'constant')
+    xc = np.abs(signal.correlate(B1, B2, mode='valid'))
+    return (np.argmax(xc) - nl)*nd
+
+def channel_preprocessing(sig, dec, fc, Fs):
+    '''deinterleave IQ samples, tune to channel frequency and decimate'''
+    IQ = deinterleave_IQ(sig)
+    IQ_tuned = frequency_shift(IQ, fc, Fs)
+    IQd = decimate(IQ_tuned, dec)
+    return IQd
