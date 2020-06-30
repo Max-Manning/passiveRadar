fb008603be76fe40c7b4d3b623bc532bc3103e86
Max Manning <macksmanning@gmail.com>
2020-06-29 20:16:27 -0700

    added file descriptions

--- range_doppler_plot.py
+++ range_doppler_plot.py
@@ -1,3 +1,6 @@
+''' Plotting script for passive radar data. Plots a sequence of range-doppler 
+    maps as separate images.'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
@@ -10,38 +13,41 @@ from passiveRadar.config_params import getConfigParams
 
 if __name__ == "__main__":
 
-    ## plot a sequence of passive radar range-doppler maps
     config_fname = "PRconfig.yaml"
     config = getConfigParams(config_fname)
     xambgfile = config['outputFile']
-    
     f = h5py.File(xambgfile, 'r')
     xambg = np.abs(f['/xambg'])
     Nframes = xambg.shape[2]
     f.close()
 
-    # CFAR filter each frame using a 2D kernel
+    # Apply a CFAR filter each frame
+    # (btw this step is pretty slow so if you're running this a bunch of times you
+    # may want to make an intermediate save point after this)
     CF = np.zeros(xambg.shape)
     for i in range(Nframes):
         CF[:,:,i] = CFAR_2D(xambg[:,:,i], 18, 4)
 
-    # make sure the save directory exists
+    # make sure the save directory exists (create it if not)
     savedir = os.path.join(os.getcwd(),  "IMG")
     if not os.path.isdir(savedir):
         os.makedirs(savedir)
 
-    # plot each frame
+    # loop over frames
     for kk in range(Nframes):
 
+        # add a digital phosphor effect to make targets easier to see
         data = persistence(CF, kk, 20, 0.90)
         data = np.fliplr(data.T) # get the orientation right
 
+        # get the save name for this frame
         svname = os.path.join(savedir, 'img_' + "{0:0=3d}".format(kk) + '.png')
         
-        # svname = '.\\IMG4\\img_' + "{0:0=3d}".format(kk) + '.png'
+        # make a figure
         figure = plt.figure(figsize = (8, 4.5))
 
-        # get max and min values for color map
+        # get max and min values for the color map (this is ad hoc, change as
+        # u see fit)
         vmn = np.percentile(data.flatten(), 1)
         vmx = 1.8*np.percentile(data.flatten(),99)
 
@@ -54,7 +60,6 @@ if __name__ == "__main__":
 
         plt.ylabel('Bistatic Range (km)')
         plt.xlabel('Doppler Shift (Hz)')
-        
         plt.tight_layout()
         plt.savefig(svname, dpi=200)
         plt.close()
